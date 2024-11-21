753aec5798bb12f73d5348316d9115740af05f8a
Rafael Pereira <r4f4santoss@gmail.com>
2024-11-20 21:55:15 -0300

    primeiro commit

---
+++ database/migrations/2024_11_08_233339_create_produto_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produto', function (Blueprint $table) {
+            $table->id();
+            $table->string("nomeProduto", 100);
+            $table->string("descricaoProduto", 255);
+            $table->unsignedBigInteger("idCategoria");
+            $table->foreign("idCategoria")->references("id")->on("categoria");
+            $table->decimal("precoProduto", 10, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produto');
+    }
+};
